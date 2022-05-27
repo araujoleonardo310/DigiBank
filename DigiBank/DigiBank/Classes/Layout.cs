efcfdd6760bc342312e71d3d5c00186a2178fa74
araujoleonardo310 <araujoleonardo310@gmail.com>
2022-05-27 14:23:49 -0300

    feat(criar conta): add tela criar conta de usuário
    Criada tela de cadastrar usuário

--- DigiBank/DigiBank/Classes/Layout.cs
+++ DigiBank/DigiBank/Classes/Layout.cs
@@ -8,13 +8,51 @@ namespace DigiBank
 {
     public class Layout
     {
+        private static int opcao = 0;
+
         public static void TelaPrincipal()
         {
             Console.BackgroundColor = ConsoleColor.Cyan;
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Clear();
 
+            Console.WriteLine("                             ");
+            Console.WriteLine(" Digite a Opção Desejada :   ");
+            Console.WriteLine(" =========================   ");
+            Console.WriteLine(" 1 - Criar Conta             ");
+            Console.WriteLine(" =========================   ");
+            Console.WriteLine(" 2 - Entrar com CPF e Senha  ");
+            Console.WriteLine(" =========================   ");
 
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    TelaCriarConta();
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 2");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção Inválida");
+                    break;
+            }
+
+        }
+
+        private static void TelaCriarConta()
+        {
+            Console.Clear();
+
+            Console.WriteLine("                             ");
+            Console.WriteLine(" Digite seu nome :           ");
+            Console.WriteLine(" =========================   ");
+            Console.WriteLine(" Digite o CPF                ");
+            Console.WriteLine(" =========================   ");
+            Console.WriteLine(" Digite sua senha            ");
+            Console.WriteLine(" =========================   ");
 
 
         }
